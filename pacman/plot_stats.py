8fb90779ad3a002359996a57e4a785f5e5ad77a3
Unknown <pietro.spa@gmail.com>
2017-12-08 23:09:03 +0100

    now plots both stats and loss

--- pacman/plot_stats.py
+++ pacman/plot_stats.py
@@ -3,14 +3,30 @@
 import matplotlib.pyplot as plt
 
 f = open('stats', 'r')
-it = []
+it_q = []
 q = []
+it_loss = []
+loss = []
 
 for line in f:
     tmp = line.split(' ')
-    it.append(int(tmp[0]))
+    it_q.append(int(tmp[0]))
     q.append(float(tmp[1]))
 
-plt.plot(it, q)
-plt.grid(True)
+f.close()
+f = open('loss', 'r')
+for line in f:
+    tmp = line.split(' ')
+    it_loss.append(int(tmp[0]))
+    loss.append(float(tmp[1]))
+
+f.close()
+
+f, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+ax1.plot(it_q, q)
+ax1.set_title('Mean max q-value')
+ax2.plot(it_loss, loss)
+ax2.set_title('Loss')
 plt.show()
+
+
